6dcd4108b0e63c6063ea12ed5c91ae74331b1d21
Ali_Elsisi <mahmodelsisi77@gmail.com@gmail.com>
2025-12-17 07:39:02 +0200

    edit a err handle in mongodb

--- index.js
+++ index.js
@@ -33,13 +33,9 @@ app.use( cookieParser() );
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Error connecting to MongoDB:", err));
-
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 app.use(cors({
   origin: "http://localhost:3000", // لو شغال محلي
