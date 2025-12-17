704e340fa4b9cbd08d6020f8f77715ecd4640218
Ali_Elsisi <mahmodelsisi77@gmail.com@gmail.com>
2025-12-17 07:52:31 +0200

    edit creat token in cookie

--- routers/usersRouters.js
+++ routers/usersRouters.js
@@ -19,12 +19,13 @@ export const RegisterUser = async ( req, res ) => {
             passwordHash: hashedPass,
         } )
         const token = createToken( user )
-            res.cookie("token", token, {
-                httpOnly: true,
-                secure: true,   // لازم HTTPS
-                sameSite: "none", // للسماح بـ cross-site
-                maxAge: 7 * 24 * 60 * 60 * 1000
-            });
+           res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,   // لازم HTTPS
+  sameSite: "none", // للسماح بـ cross-site
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
 
         res.status(201).json({ message: "User registered successfully", token });
     } catch (err) {
@@ -53,11 +54,12 @@ export const LogInUser =  async ( req, res ) => {
         }
         const token = createToken( user );
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,   // لازم HTTPS
-            sameSite: "none", // للسماح بـ cross-site
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
+  httpOnly: true,
+  secure: true,   // لازم HTTPS
+  sameSite: "none", // للسماح بـ cross-site
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
 
         res.status(200).json({message: "User logged in successfully", token})
 
